f993c40aec34c506031da5d6545c559b3d6effd2
ZxZ6389 <694118432@qq.com>
2016-02-26 07:27:13 +0800

    return 02
    return 02

--- src/com/atguigu/git/test/Hello.java
+++ src/com/atguigu/git/test/Hello.java
@@ -5,6 +5,8 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("欢迎来到硅谷!");
 		System.out.println("GitHub to 01!");
+		System.out.println("GitHub to 02!");
+		
 	}
 
 }
